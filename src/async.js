8f8cff4f9ed30cc8e62c28e5a5796a2849134e2e
Joshua Thornton <joshua.thornton@heg.com>
2017-05-27 19:56:11 +0100

    Re-fetches when the src changes.

--- src/async.js
+++ src/async.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { lifecycle, withState, compose, withProps } from 'recompose';
 import axios from 'axios';
 
+const fetchSVGContent = (src) => axios.get(src);
+
 export default compose(
   withState(
     'rawSrc',
@@ -10,8 +12,14 @@ export default compose(
   ),
   lifecycle({
     componentDidMount: function() {
-      axios.get(this.props.src).then((response) => this.props.setRawSrc(response.data));
-    }
+      fetchSVGContent(this.props.src).then((response) => this.props.setRawSrc(response.data));
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+      if(nextProps.src !== this.props.src) {
+        fetchSVGContent(nextProps.src).then((response) => nextProps.setRawSrc(response.data));
+      }
+    },
   }),
   withProps((ownerProps) => ({
     src: ownerProps.rawSrc,
